85fc27ac204e34a7f98d5bc4489c4bd1fd9bd5c7
Alexander Wu <alexander.wu7@gmail.com>
2026-04-07 22:24:14 -0700

    Update jobs.duckdb 2026-04-07

--- job_search/resume.py
+++ job_search/resume.py
@@ -263,9 +263,10 @@ if __name__ == "__main__":
     # P_resume = P_INTERIM / 'AW_Tempus_Resume.md'
     # P_resume = P_INTERIM / 'AW_Komodo_Resume.md'
     # P_resume = P_INTERIM / 'AW_Paypal_Resume.md'
-    P_resume = P_INTERIM / 'AW_Resume.md'
-    # convert_resume(P_resume, keep_docx=False, pagebreak=True)
+    # P_resume = P_INTERIM / 'AW_Resume.md'
+    P_resume = P_INTERIM / 'AW_Q2BI_Resume.md'
     convert_resume(P_resume, keep_docx=False, pagebreak=False)
+    # convert_resume(P_resume, keep_docx=True, pagebreak=False)
     # convert_resume(P_resume, keep_docx=True, pagebreak=True)
     # print(f'swriter --headless --convert-to pdf "data/interim/{P_resume.stem}.docx" --outdir data/processed/')
     #swriter --headless --convert-to pdf "data/interim/AW_Genentech_MDAE_Resume.docx" --outdir data/processed/
